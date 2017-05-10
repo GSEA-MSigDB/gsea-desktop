dcb660a7c73e2706aef72540e7774be58b9fe180
eby <eby@broadinstitute.org>
2017-05-11 03:20:00 +0900

    Modified the update message. Modified to allow Check For Update to detect a patchLevel change. Modified the website query string to match expected server-side params.

--- src/xapps/gsea/UpdateChecker.java
+++ src/xapps/gsea/UpdateChecker.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.log4j.Logger;
 import org.genepattern.uiutil.UIUtil;
@@ -27,16 +28,17 @@ public class UpdateChecker {
     private static boolean MAKE_GSEA_UPDATE_CHECK = BooleanUtils.toBoolean(System.getProperty("MAKE_GSEA_UPDATE_CHECK", "true"))
             && XPreferencesFactory.kMakeGseaUpdateCheck.getBoolean();
 
+    private static final String UPDATE_CHECK_EXTRA_PROJECT_INFO = System.getProperty("UPDATE_CHECK_EXTRA_PROJECT_INFO", "GSEA");
+
     public static final void oneTimeGseaUpdateCheck(Component parent) {
         if (!XPreferencesFactory.kOnlineMode.getBoolean()) {
             klog.info("Currently running disconnected from the internet: skipping GSEA update check.");
         } else if (MAKE_GSEA_UPDATE_CHECK) {
 
-            // Run the update check here. This will give us a bundle (Props? JSON?) with info on the latest version available
-            // from the website. Suppress exceptions; this check should be silent and not hinder the program.
             try {
                 String versionQueryString = GSEA_UPDATE_CHECK_URL + "?currentVersion="
-                        + GseaFijiTabsApplicationFrame.buildProps.getProperty("build.version", "not_available");
+                        + GseaFijiTabsApplicationFrame.buildProps.getProperty("build.version", "not_available")
+                        + "&extraProjectInfo=" + UPDATE_CHECK_EXTRA_PROJECT_INFO;
 
                 URL url = new URL(versionQueryString);
                 URLConnection connection = url.openConnection();
@@ -44,28 +46,32 @@ public class UpdateChecker {
                 connection.setReadTimeout(20000);
                 String versionCheckInfo = IOUtils.toString(connection.getInputStream(), (Charset) null);
 
-                // Parse this info (Props? JSON?). We're just pretending for now.
-                int thisMajor = NumberUtils.toInt(GseaFijiTabsApplicationFrame.buildProps.getProperty("build.major", "3"), 3);
-                int thisMinor = NumberUtils.toInt(GseaFijiTabsApplicationFrame.buildProps.getProperty("build.minor", "0"), 0);
-                Properties currentGseaVersionProps = parseGseaVersionInfo(versionCheckInfo);
-                int currMajor = NumberUtils.toInt(currentGseaVersionProps.getProperty("build.major", "3"), 3);
-                int currMinor = NumberUtils.toInt(currentGseaVersionProps.getProperty("build.minor", "0"), 0);
+                int currMajor = NumberUtils.toInt(GseaFijiTabsApplicationFrame.buildProps.getProperty("build.major", "3"), 3);
+                int currMinor = NumberUtils.toInt(GseaFijiTabsApplicationFrame.buildProps.getProperty("build.minor", "0"), 0);
+                int currPatch = NumberUtils.toInt(GseaFijiTabsApplicationFrame.buildProps.getProperty("build.patchLevel", "0"), 0);
+                String currVersion = GseaFijiTabsApplicationFrame.buildProps.getProperty("build.version", "3.0");
+                Properties latestGseaVersionProps = parseGseaVersionInfo(versionCheckInfo);
+                int latestMajor = NumberUtils.toInt(latestGseaVersionProps.getProperty("build.major", "3"), 3);
+                int latestMinor = NumberUtils.toInt(latestGseaVersionProps.getProperty("build.minor", "0"), 0);
+                int latestPatch = NumberUtils.toInt(latestGseaVersionProps.getProperty("build.patchLevel", "0"), 0);
 
                 // Compare the current website version info to the local info and if so display/log a message.
-                if (compareThisVersionToCurrent(thisMajor, currMajor, thisMinor, currMinor)) {
-                    String currentVersion = currentGseaVersionProps.getProperty("build.version");
-                    String currentTimestamp = currentGseaVersionProps.getProperty("build.timestamp");
-                    String message = "A newer version of GSEA (" + currentVersion + ") is available " + IOUtils.LINE_SEPARATOR
-                            + "from http://gsea-msigdb.org/gsea/downloads.jsp" + IOUtils.LINE_SEPARATOR + "(build date: "
-                            + currentTimestamp + ")";
+                if (newerVersionExists(currMajor, latestMajor, currMinor, latestMinor, currPatch, latestPatch)) {
+                    String latestVersion = latestGseaVersionProps.getProperty("build.version");
+                    String latestTimestamp = latestGseaVersionProps.getProperty("build.timestamp");
+                    String message = "Your current version of GSEA is " + currVersion + ". A newer version (" + latestVersion
+                            + ") is available." + IOUtils.LINE_SEPARATOR
+                            + "To update, please download from http://gsea-msigdb.org/gsea/downloads.jsp" + IOUtils.LINE_SEPARATOR
+                            + "(build date: " + latestTimestamp + ")";
 
                     klog.info(message);
+                    klog.info("Note: GenePattern users should update through GenePattern.");
                     if (parent != null) {
                         UIUtil.showMessageDialog(parent, message);
                     }
                 }
             } catch (Throwable t) {
-                // Silently swallow exceptions.
+                // Silently swallow exceptions. Update check failure should not affect normal operation.
             }
         }
 
@@ -86,7 +92,9 @@ public class UpdateChecker {
         return currentGseaVersionProps;
     }
 
-    private static final boolean compareThisVersionToCurrent(int thisMajor, int currMajor, int thisMinor, int currMinor) {
-        return (currMajor > thisMajor) || (currMajor == thisMajor && currMinor > thisMinor);
+    private static final boolean newerVersionExists(int currMajor, int latestMajor, int currMinor, int latestMinor, int currPatch,
+            int latestPatch) {
+        return (latestMajor > currMajor) || (latestMajor == currMajor && latestMinor > currMinor)
+                || (latestMajor == currMajor && latestMinor == currMinor && latestPatch > currPatch);
     }
 }
