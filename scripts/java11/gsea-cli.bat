64af3f2d3816b80e0c218cf30e0e180ddd41b511
eby <eby@broadinstitute.org>
2020-02-18 16:42:06 +0900

    Additional Windows launcher fix
    Missed a couple of path references in the previous check-in

--- scripts/java11/gsea-cli.bat
+++ scripts/java11/gsea-cli.bat
@@ -1,3 +1,3 @@
 setlocal
 
-start jdk-11\bin\javaw -showversion --module-path=%BatchPath%\modules -Xmx4g -Djava.awt.headless=true @%BatchPath%\gsea.args --patch-module=jide.common=%BatchPath%\lib\jide-components-3.7.4.jar;%BatchPath%\lib\jide-dock-3.7.4.jar;%BatchPath%\lib\jide-grids-3.7.4.jar --module=org.gsea_msigdb.gsea/xapps.gsea.CLI  %*
+start jdk-11\bin\javaw -showversion --module-path=modules -Xmx4g -Djava.awt.headless=true @gsea.args --patch-module=jide.common=lib\jide-components-3.7.4.jar;lib\jide-dock-3.7.4.jar;lib\jide-grids-3.7.4.jar --module=org.gsea_msigdb.gsea/xapps.gsea.CLI  %*
